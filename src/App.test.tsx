495f22e5363ba14b6eb0752983625f31974306be
Oliver Forral <intrepion@gmail.com>
2022-11-23 19:52:02 -0700

    Pared code down to basics.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders zero2prod', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/zero2prod/i);
   expect(linkElement).toBeInTheDocument();
 });
