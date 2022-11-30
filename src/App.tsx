6a2ef06ea6f6fcb0dd6274308a4b49c9b0182d06
Oliver Forral <intrepion@gmail.com>
2022-11-30 14:33:02 -0700

    Added a login.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Zero2Prod from "./Zero2Prod";
 
 function App() {
   return (
     <div>
       <h1>zero2prod</h1>
+      <Zero2Prod />
     </div>
   );
 }
---
+++ src/Zero2Prod.tsx
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { useState } from "react";
+
+const Zero2Prod = () => {
+    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+
+    const changePassword = (event: any) => {
+        setPassword(event.target.value);
+    }
+    const changeUsername = (event: any) => {
+        setUsername(event.target.value);
+    }
+
+    const login = () => {
+        const params = new URLSearchParams();
+
+        params.append('password', password);
+        params.append('username', username);
+
+        axios.post('http://localhost:8000/login', params)
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
+
+    return (
+        <div>
+            <label>
+                Username{" "}
+                <input type="text" onChange={changeUsername} placeholder="Enter Username" name="username" />{" "}
+            </label>
+            <label>
+                Password{" "}
+                <input type="password" onChange={changePassword} placeholder="Enter Password" name="password" />{" "}
+            </label>
+            <button onClick={login}>Login</button>
+        </div>
+      );
+};
+
+export default Zero2Prod;
